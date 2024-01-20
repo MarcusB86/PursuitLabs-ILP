7cb57efa6907e948bd5ac0e6990ff5508dbec4e6
Marcus Browne <marcusb08@gmail.com>
2024-01-20 11:46:26 -0500

    comments in code

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
 function sortWithBubbleSort(arr) {
-
+     // Ascending order
     for (let i = 0; i < arr.length; i++) {
+        //Outer for loop to keep track of each iteration we're on.
         for (let j = 0; j < arr.length; j++) {
+            //Inner for loop starts comparing the elements adjacent to each other.
             if (arr[j] > arr[j + 1]) {
+                // Checks if the current element is larger than the element adjacent.
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                // Swap the larger element with the smaller element.
             }
         }
     }
     return arr;
 }
-console.log(sortWithBubbleSort([6, 5, 3, 1, 8, 7, 2, 4]));
-
-
-function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-        }
-    }
-    return arr;
-}
-console.log(bubbleSort([-4, -6, -9, 4, 7, 2]));
-
-
-function sortWithBubble(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j] > arr[j +1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-        }
-    }
-    return arr
-}
-console.log(sortWithBubble([9,3,6,5,3,6,7,2,34,-6,2,-7,-3,-86,-3,77, 98,485,47,237,495,142,695,2974,3847]));
+console.log(sortWithBubbleSort([6, 5, 3, 1, 8, 7, 2, 4]));// unsorted array of numbers
+console.log(sortWithBubbleSort([-4,-5,-3,-8,-55,-99,-3.-1,-54,-23,-12,-98,-49]));// unsorted array with negative numbers
+console.log(sortWithBubbleSort(["Hello", "Bubble", "Sort", "The", "Then","Alter","Why","Double"]));// array of strings
